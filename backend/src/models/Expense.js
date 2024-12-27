e5ed3e652942e359f1a72a4c9c9734f6853da151
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-12-28 00:41:36 +0530

    add expense schema

---
+++ backend/src/models/Expense.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const expenseSchema = new mongoose.Schema({
+    amount: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['Food', 'Transportation', 'Entertainment', 'Utilities', 'Health', 'Other'],
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+        enum: ['cash', 'credit'],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+const Expense = mongoose.model('Expense', expenseSchema);
+
+export default Expense;
